f0837545e2bc305c28340147b648157131c244c8
luigiikk <luigimee15@gmail.com>
2024-08-14 22:21:08 -0300

    feat: connection with PostgreSQL

---
+++ create-table.js
@@ -0,0 +1,15 @@
+import sql  from "./db.js";
+
+// sql`DROP TABLE IF EXISTS videos;`.then(()=> {
+//     console.log("TABELA APAGADA")
+// })
+
+sql`CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    duration INTEGER
+);
+`.then(() => {
+    console.log('Tabela criada com sucesso')
+});
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import { fastify } from "fastify";
-import { DatabaseMemory } from "./databasememory.js";
+// import { DatabaseMemory } from "./databasememory.js";
+import { DatabasePostgres } from "./database-postgres.js";
 
 const server = fastify();
 
-const database = new DatabaseMemory();
+// const database = new DatabaseMemory();
+const database = new DatabasePostgres();
 
-server.post("/videos", (request, reply) => {
+server.post("/videos", async (request, reply) => {
   const { title, description, duration } = request.body;
 
-  database.create({
+  await database.create({
     title,
     description,
     duration,
@@ -17,10 +19,10 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send(); //201 => algo foi criado
 });
 
-server.get("/videos", (request) => {
+server.get("/videos", async (request) => {
   const search = request.query.search;
 
-  const videos = database.list(search);
+  const videos = await database.list(search);
 
   return videos;
 });
